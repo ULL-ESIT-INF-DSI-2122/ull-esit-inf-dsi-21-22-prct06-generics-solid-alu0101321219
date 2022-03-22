fbe8ce54b8fac6acb5c4d60b9ca0fe891d3e01f3
Adrián González Galván <alu0101321219@ull.edu.es>
2022-03-22 17:14:06 +0000

    Agregamos fichero spec del ejercicio 1

---
+++ tests/ejercicio-1.spec.ts
@@ -0,0 +1,11 @@
+import 'mocha';
+import {expect} from 'chai';
+import {Fighter} from './../src/ejercicio-1/fighter';
+
+describe('Ejercicio 1 - El combate definitivo', () => {
+  describe('Fighter', () => {
+    it('Existe una clase llamada Fighter', () => {
+      expect(Fighter != undefined).to.be.true;
+    });
+  });
+});
--- tests/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import 'mocha';
-import {expect} from 'chai';
-import {add} from './../src/index';
-
-describe('Function add', () => {
-  it('add(1, 2) returns 3', () => {
-    expect(add(1, 2)).to.be.equal(3);
-  });
-});
