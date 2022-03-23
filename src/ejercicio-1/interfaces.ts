ef141a1dff6acd6b10d1e15591470fe292d16e39
Adrián González Galván <alu0101321219@ull.edu.es>
2022-03-23 16:56:58 +0000

    Comentamos interfaces y métodos de las mismas

--- src/ejercicio-1/interfaces.ts
+++ src/ejercicio-1/interfaces.ts
@@ -3,9 +3,22 @@
  * de impresión por pantalla.
  */
 export interface Printable {
+  /**
+   * Imprime por pantalla las características de un objeto, en nuestro caso
+   * de un contendiente.
+   */
   print(): void;
 }
 
+/**
+ * Interfaz empleada para forzar la existencia de una 'catching phrase'
+ * para cada uno de los contendientes de nuestro universo.
+ */
 export interface CatchingPhrase {
+  /**
+   * Devuelve una cadena con la 'catching phrase' de un determinado contendiente
+   * de nuestro universo.
+   * @returns Cadena con la 'catching phrase' correspondiente.
+   */
   catchingPhrase(): string;
 }
