952f72b7e4c72dce34d7cb2041554eab2296d2c9
Adrián González Galván <alu0101321219@ull.edu.es>
2022-03-25 17:08:15 +0000

    Modificamos streamable

--- src/ejercicio-2/streamable.ts
+++ src/ejercicio-2/streamable.ts
@@ -1,38 +1,10 @@
-export interface SearchableStreamable<T> {
-  /**
-   * Permite buscar un conjunto de emisiones concretas a través de su nombre.
-   * @param name Nombre de la emision a buscar.
-   * @returns Subconjunto de emisiones que poseen ese nombre.
-   */
-   searchByName(name: string): T[];
-
-   /**
-    * Permite buscar un conjunto de emisiones concreta a partir de su año de
-    * publicación.
-    * @param year Año de publicación de una emisión.
-    * @returns Subconjunto de emisiones con dicho año de publicación.
-    */
-   searchbyYear(year: number): T[];
-
-   /**
-    * Permite buscar un conjunto de emisiones concreta a partir de una categoría
-    * especificada por parámetro.
-    * @param category Categoría a especificar.
-    * @returns Subconjunto de emisiones que cumplen dicha categoría.
-    */
-   searchByCategory(category: string): T[];
-}
+import {SearchableStreamable} from "./searchablestreamable";
 
 /**
  * Especifica las propiedades y métodos con los que debería contar
  * una colección de emisiones concreta (ej: colección de series)
  */
 export interface Streamable<T> extends SearchableStreamable<T> {
-  /**
-   * Atributo para almacenar la colección de emisiones.
-   */
-  collection: T[];
-
   /**
    * Devuelve la colleción de emisiones
    * @return Colección completa de emisiones.
