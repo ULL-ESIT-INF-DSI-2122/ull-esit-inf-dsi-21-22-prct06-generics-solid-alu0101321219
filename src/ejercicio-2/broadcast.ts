a17ec0fd036f8e05285f6fba9b3e6f8f107dc943
Adrián González Galván <alu0101321219@ull.edu.es>
2022-03-25 16:37:07 +0000

    Añadimos alias 'Category'

--- src/ejercicio-2/broadcast.ts
+++ src/ejercicio-2/broadcast.ts
@@ -4,6 +4,14 @@
 export type Puntuation = 1 | 2 | 3 | 4 | 5;
 
 /**
+ * Conjunto de categorías posibles que puede tener una determinada emisión.
+ */
+export type Category = 'action' | 'animation' | 'anime' | 'adventure' | 'war' |
+  'science fiction' | 'comedy' | 'crime' | 'drama' | 'family' | 'fantasy' |
+  'history' | 'for children' | 'mistery' | 'music' | 'romance' |
+  'superheroes' | 'suspense' | 'terror' | 'sports';
+
+/**
  * Recoge las características que tendría una emisión cualquiera de una
  * plataforma de video en streaming.
  */
@@ -26,7 +34,7 @@ export interface Broadcast {
   /**
    * Categorías en las que se puede ubicar dicha emisión.
    */
-  categories: string[];
+  categories: Category[];
 
   /**
    * Año de publicación de la emisión.
